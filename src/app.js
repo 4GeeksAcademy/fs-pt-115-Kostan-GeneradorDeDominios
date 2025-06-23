e8bb040e7305a6dd4195f64d774f4523795f52d6
K0ST4N <kostantinatanasov11@gmail.com>
2025-06-23 18:03:38 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,26 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+import "bootstrap";
+import "./style.css";
+
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  //write your code here
+  console.log("Hello Rigo from the console!");
+};
+ let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let o = 0; o < adj.length; o++) {
+      for (let p = 0; p < noun.length; p++) {
+       let element = pronoun[i] + adj[o] + noun[p] + ".com";
+        console.log(element);
+      }
+    } 
+  }
