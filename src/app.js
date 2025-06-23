4833ed6902ce607e19c3a35f1fbf0032271dab10
K0ST4N <kostantinatanasov11@gmail.com>
2025-06-23 18:35:26 +0000

    Actualizado

--- src/app.js
+++ src/app.js
@@ -9,17 +9,6 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
  let pronoun = ['the', 'our'];
   let adj = ['great', 'big'];
   let noun = ['jogger', 'racoon'];
