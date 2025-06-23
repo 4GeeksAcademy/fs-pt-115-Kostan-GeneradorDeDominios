fd0c1221aa180c05ad82632e6c47850327f3cce0
K0ST4N <kostantinatanasov11@gmail.com>
2025-06-23 18:13:49 +0000

    Actualizado

--- src/app.js
+++ src/app.js
@@ -24,10 +24,22 @@ window.onload = function() {
   let adj = ['great', 'big'];
   let noun = ['jogger', 'racoon'];
 
+  let extensiones = ['.com', '.net', '.org', '.io'];
+
+  let domainHacks= extensiones.map(ext => {
+    return pronoun.map(pr => {
+      return adj.map(ad => {
+        return noun.map(n => {
+          return pr + ad + n + ext;
+        });
+      });
+    });
+  }).flat(3);
+
   for (let i = 0; i < pronoun.length; i++) {
     for (let o = 0; o < adj.length; o++) {
       for (let p = 0; p < noun.length; p++) {
-       let element = pronoun[i] + adj[o] + noun[p] + ".com";
+       let element = pronoun[i] + adj[o] + noun[p] + extensiones[Math.floor(Math.random() * extensiones.length)];
         console.log(element);
       }
     } 
